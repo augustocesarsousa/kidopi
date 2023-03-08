c75467cc3e62c9e1e122e8ba00c0552b2857eb82
Augusto Cesar <acs-03@hotmail.com>
2023-03-08 18:01:48 -0300

    Pegando o a última consulta e exibindo no footer

--- conn.php
+++ conn.php
@@ -1,7 +1,16 @@
 <?php
-$conn = new mysqli('127.0.0.1','root','root@123','kidopi');
-if ($conn->connect_error) {
-    echo connect_error;
-    die('Error : ('. $conn->connect_error .') '. $conn->connect_error);
+$host = "127.0.0.1";
+$user = "root";
+$password = "root@123";
+$dbname = "kidopi";
+$port = 3306;
+
+try{
+    //Conexão com a porta
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=".$dbname, $user, $password);
+
+    //echo "Conexão com banco de dados realizado com sucesso!";
+}  catch(PDOException $err){
+    echo "Erro ao conectar com o banco de dados " . $err->getMessage();
 }
 ?>
--- js/scripts.js
+++ js/scripts.js
@@ -1,80 +1,113 @@
 const baseUrl = "https://dev.kidopilabs.com.br/exercicio/covid.php?";
-const selectCountry = document.getElementById("select-country");
-const loadingMessage = document.getElementById("loading-message");
-const totalCases = document.getElementById("total-cases");
-const totalDeats = document.getElementById("total-deats");
+const inputSelectCountry = document.getElementById("select-country");
+const pLoadingMessage = document.getElementById("loading-message");
+const pTotalCases = document.getElementById("total-cases");
+const pTotalDeats = document.getElementById("total-deats");
+const divTableContainer = document.getElementById("table-container");
+const pLastSeachContent = document.getElementById("last-seach-content");
+const spanLastSeachDate = document.getElementById("last-seach-date");
+const spanLastSeachCountry = document.getElementById("last-seach-country");
+const tableHeaderElements = ["Estado/Província", "Casos confirmados", "Mortes confirmadas"];
 let contTotalCasos = 0;
 let contTotalMortes = 0;
-const tableContainer = document.getElementById("table-container");
-const tableHeaderElements = ["Estado/Província", "Casos confirmados", "Mortes confirmadas"];
 
-async function getByCountry() {
+const selectCountry = async () => {
 
-    let country = selectCountry.options[selectCountry.selectedIndex].value;
+    let country = inputSelectCountry.options[inputSelectCountry.selectedIndex].value;
     if (country !== "") {
 
-        const table = document.createElement("table");
-        const tableHeader = document.createElement("thead"); ""
-        const tableBody = document.createElement("tbody");
+        pTotalCases.classList.add("hide");
+        pTotalDeats.classList.add("hide");
+        divTableContainer.classList.add("hide");
+        pLoadingMessage.classList.remove("hide");
 
-        totalCases.classList.add("hide");
-        totalDeats.classList.add("hide");
-        tableContainer.classList.add("hide");
-        loadingMessage.classList.remove("hide");
+        const data = await getByCountryAPI(country);
+        createTable(data);
 
-        const response = await fetch(baseUrl + "pais=" + country);
-        const data = await response.json();
+        pTotalCases.classList.remove("hide");
+        pTotalDeats.classList.remove("hide");
+        divTableContainer.classList.remove("hide");
+        pLoadingMessage.classList.add("hide");
 
-        tableHeaderElements.map((element) => {
-            const th = document.createElement("th");
-            th.innerText = element;
-
-            tableHeader.appendChild(th);
-        })
-
-        for (const key in data) {
-            const td1 = document.createElement("td");
-            const td2 = document.createElement("td");
-            const td3 = document.createElement("td");
-            const tr = document.createElement("tr");
-
-            td1.innerText = data[key].ProvinciaEstado;
-            td2.innerText = data[key].Confirmados;
-            td3.innerText = data[key].Mortos;
-
-            contTotalCasos += data[key].Confirmados;
-            contTotalMortes += data[key].Mortos;
-
-            tr.appendChild(td1);
-            tr.appendChild(td2);
-            tr.appendChild(td3);
-
-            tableBody.appendChild(tr);
-        }
-
-        totalCases.classList.remove("hide");
-        totalDeats.classList.remove("hide");
-        tableContainer.classList.remove("hide");
-        loadingMessage.classList.add("hide");
-
-        totalCases.innerText = `Total de casos: ${contTotalCasos}`;
-        totalDeats.innerText = `Total de mortes: ${contTotalMortes}`;
-
-        table.appendChild(tableHeader);
-        table.appendChild(tableBody);
-
-        tableContainer.replaceChildren();
-        tableContainer.appendChild(table);
-
-        const formData = new FormData();
-        formData.append('country', country);
-
-        const teste = await fetch("save.php", {
-            method: "POST",
-            body: formData
-        });
-
-        const responseSave = await teste.json();
-        console.log(responseSave.message);
+        saveCountry(country);
     }
 }
+
+const getByCountryAPI = async (country) => {
+
+    const response = await fetch(baseUrl + "pais=" + country);
+    return response.json();
+}
+
+const saveCountry = async (country) => {
+
+    const formData = new FormData();
+    formData.append('country', country);
+
+    const response = await fetch("save_country.php", {
+        method: "POST",
+        body: formData
+    });
+
+    const result = await response.json();
+    console.log(result.message);
+
+}
+
+const getLastSeach = async () => {
+
+    const response = await fetch("get_last_seach.php");
+    const data = await response.json();
+    if (data.status) {
+        const date = new Date(data.date);
+        spanLastSeachDate.innerText = date.toLocaleString();
+        spanLastSeachCountry.innerText = data.country;
+        pLastSeachContent.classList.remove("hide");
+    }
+
+}
+
+const createTable = (data) => {
+
+    const table = document.createElement("table");
+    const tableHeader = document.createElement("thead");
+    const tableBody = document.createElement("tbody");
+
+    tableHeaderElements.map((element) => {
+        const th = document.createElement("th");
+        th.innerText = element;
+
+        tableHeader.appendChild(th);
+    })
+
+    for (const key in data) {
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const td3 = document.createElement("td");
+        const tr = document.createElement("tr");
+
+        td1.innerText = data[key].ProvinciaEstado;
+        td2.innerText = data[key].Confirmados;
+        td3.innerText = data[key].Mortos;
+
+        contTotalCasos += data[key].Confirmados;
+        contTotalMortes += data[key].Mortos;
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+
+        tableBody.appendChild(tr);
+    }
+
+    pTotalCases.innerText = `Total de casos: ${contTotalCasos}`;
+    pTotalDeats.innerText = `Total de mortes: ${contTotalMortes}`;
+
+    table.appendChild(tableHeader);
+    table.appendChild(tableBody);
+
+    divTableContainer.replaceChildren();
+    divTableContainer.appendChild(table);
+}
+
+getLastSeach();
