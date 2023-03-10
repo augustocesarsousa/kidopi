8e716d7149e76725a181b291438567a15d567ef6
Augusto Cesar <acs-03@hotmail.com>
2023-03-10 15:47:24 -0300

    Estilizando página home

--- frontend/js/index-main.js
+++ frontend/js/index-main.js
@@ -1,12 +1,14 @@
 import { getByCountryKidopi } from "./services/kidopi-api-service.js";
 import { getSelectCountryBackend, getLastSearchBackend, saveCountryBackend } from "./services/backend-api-service.js";
 
+const dataContainer = document.getElementById("data-table-container");
 const inputSelectCountry = document.getElementById("select-country");
 const btnSelectCountry = document.getElementById("select-country-btn");
 const spanSelectCountryMessage = document.getElementById("select-country-message");
 const pLoadingMessage = document.getElementById("loading-message");
 const pTotalCases = document.getElementById("total-cases");
 const pTotalDeaths = document.getElementById("total-deaths");
+const pTotalFee = document.getElementById("total-fee");
 const divTableContainer = document.getElementById("table-container");
 const pLastSearchContent = document.getElementById("last-search-content");
 const spanLastSearchDate = document.getElementById("last-search-date");
@@ -14,6 +16,7 @@ const spanLastSearchCountry = document.getElementById("last-search-country");
 const tableHeaderElements = ["Estado/Província", "Casos confirmados", "Mortes confirmadas", "Taxa (%)"];
 let totalCases = 0;
 let totalDeaths = 0;
+let totalFee = 0;
 
 btnSelectCountry.addEventListener("click", async () => {
 
@@ -24,9 +27,11 @@ btnSelectCountry.addEventListener("click", async () => {
 
         pTotalCases.classList.add("hide");
         pTotalDeaths.classList.add("hide");
+        pTotalFee.classList.add("hide");
         divTableContainer.classList.add("hide");
         pLoadingMessage.classList.remove("hide");
-        pLastSearchContent.classList.add("hide");
+        // pLastSearchContent.classList.add("hide");
+        dataContainer.classList.add("hide");
 
         const result = await getByCountryKidopi(country);
         createTableSearch(result);
@@ -34,8 +39,10 @@ btnSelectCountry.addEventListener("click", async () => {
 
         pTotalCases.classList.remove("hide");
         pTotalDeaths.classList.remove("hide");
+        pTotalFee.classList.remove("hide");
         divTableContainer.classList.remove("hide");
         pLoadingMessage.classList.add("hide");
+        dataContainer.classList.remove("hide");
 
         saveCountryBackend(country);
     } else {
@@ -66,7 +73,7 @@ const createLastSearch = async () => {
         const date = new Date(result.date);
         spanLastSearchDate.innerText = date.toLocaleString("pt-BR");
         spanLastSearchCountry.innerText = result.country;
-        pLastSearchContent.classList.remove("hide");
+        // pLastSearchContent.classList.remove("hide");
     }
 }
 
@@ -74,16 +81,21 @@ const createTableSearch = (result) => {
 
     totalCases = 0;
     totalDeaths = 0;
+    totalFee = 0;
     const table = document.createElement("table");
+    table.classList.add("table-content");
     const tableHeader = document.createElement("thead");
+    const tableHeaderTR = document.createElement("tr");
     const tableBody = document.createElement("tbody");
 
     tableHeaderElements.map((element) => {
         const th = document.createElement("th");
         th.innerText = element;
 
-        tableHeader.appendChild(th);
+        tableHeaderTR.appendChild(th);
     });
+
+    tableHeader.appendChild(tableHeaderTR);
 
     for (const key in result) {
         const tdProvinciaEstado = document.createElement("td");
@@ -94,10 +106,11 @@ const createTableSearch = (result) => {
         let fee = 0;
 
         tdProvinciaEstado.innerText = result[key].ProvinciaEstado;
-        tdCases.innerText = result[key].Confirmados;
-        tdDeaths.innerText = result[key].Mortos;
+        tdCases.innerText = result[key].Confirmados.toLocaleString();
+        tdDeaths.innerText = result[key].Mortos.toLocaleString();
         if (result[key].Confirmados > 0) {
             fee = result[key].Mortos / result[key].Confirmados;
+            totalFee += fee;
         }
         tdFee.innerText = fee.toFixed(4);
 
@@ -112,8 +125,9 @@ const createTableSearch = (result) => {
         tableBody.appendChild(tr);
     }
 
-    pTotalCases.innerText = `Total de casos: ${totalCases}`;
-    pTotalDeaths.innerText = `Total de mortes: ${totalDeaths}`;
+    pTotalCases.innerText = `Total de casos: ${totalCases.toLocaleString()}`;
+    pTotalDeaths.innerText = `| Total de mortes: ${totalDeaths.toLocaleString()}`;
+    pTotalFee.innerText = `| Taxa(%): ${totalFee.toFixed(4)}`;
 
     table.appendChild(tableHeader);
     table.appendChild(tableBody);
